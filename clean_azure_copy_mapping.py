387a10461aa79baf28623f0a8428c7b882c1f269
Mirko Leccese <leccese_mirko@yahoo.it>
2022-09-18 19:01:50 +0200

    correct replace function

--- clean_azure_copy_mapping.py
+++ clean_azure_copy_mapping.py
@@ -8,8 +8,9 @@ characterToCheck = [",", ";", "{", "}", "(", ")", "\n", "\t", "=", " "]
 #characterToCheck = ",;{()}\n\t= "
 
 def replace(str, characterList):
+    x = str
     for ch in characterList:
-        x = str.replace(ch,"_")
+        x = x.replace(ch,"_")
     return x
 
 f = open(filename, 'r')
@@ -17,6 +18,8 @@ data = json.load(f)
 
 for i in data['translator']['mappings']:
     if any(char in i['source'] for char in characterToCheck):
-        i['sink'] = replace(i['sink'], characterToCheck)
-    print(i)
+        test = i['sink']
+        print(test)
+        test = replace(test, characterToCheck)
+        print(test)
 f.close()
