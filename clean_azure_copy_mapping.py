35e7b53b0a5cdbcf6c1745b52ae03933b65bb8c8
Mirko Leccese <leccese_mirko@yahoo.it>
2022-09-18 17:10:39 +0200

    add replacing function

--- clean_azure_copy_mapping.py
+++ clean_azure_copy_mapping.py
@@ -1,12 +1,22 @@
 import json
+import re
 
 # Opening JSON file
 filename = input('Filename?')
 
+characterToCheck = [",", ";", "{", "}", "(", ")", "\n", "\t", "=", " "]
+#characterToCheck = ",;{()}\n\t= "
+
+def replace(str, characterList):
+    for ch in characterList:
+        x = str.replace(ch,"_")
+    return x
+
 f = open(filename, 'r')
 data = json.load(f)
 
-for i in data['translator']:
+for i in data['translator']['mappings']:
+    if any(char in i['source'] for char in characterToCheck):
+        i['sink'] = replace(i['sink'], characterToCheck)
     print(i)
-
 f.close()
