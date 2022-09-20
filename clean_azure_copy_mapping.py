9afc1b851c647e086bc1110db1b147e32d87ee69
Mirko Leccese <leccese_mirko@yahoo.it>
2022-09-20 21:26:21 +0200

    modify replace function to clean and write output to new file

--- clean_azure_copy_mapping.py
+++ clean_azure_copy_mapping.py
@@ -7,19 +7,21 @@ filename = input('Filename?')
 characterToCheck = [",", ";", "{", "}", "(", ")", "\n", "\t", "=", " "]
 #characterToCheck = ",;{()}\n\t= "
 
-def replace(str, characterList):
-    x = str
+def clean(str, characterList):
+    x = str.strip()
     for ch in characterList:
         x = x.replace(ch,"_")
     return x
 
-f = open(filename, 'r')
-data = json.load(f)
+with open(filename, 'r') as f:
+    data = json.load(f)
 
 for i in data['translator']['mappings']:
     if any(char in i['source'] for char in characterToCheck):
-        test = i['sink']
-        print(test)
-        test = replace(test, characterToCheck)
-        print(test)
+        i['sink'] = clean(i['sink'], characterToCheck)
 f.close()
+
+filename_mod = str(filename) + '.New'
+with open(filename_mod, 'w') as f_mod:
+    json.dump(data, f_mod, indent=4)
+f_mod.close()
