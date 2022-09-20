12d8e8765a5bbb6d74d8a54b88063226d1ad9b2a
Mirko Leccese <leccese_mirko@yahoo.it>
2022-09-20 21:42:11 +0200

    add comments

--- clean_azure_copy_mapping.py
+++ clean_azure_copy_mapping.py
@@ -1,27 +1,53 @@
 import json
-import re
+import sys
 
-# Opening JSON file
-filename = input('Filename?')
-
+# Defining list of character to check in the JSON file:
 characterToCheck = [",", ";", "{", "}", "(", ")", "\n", "\t", "=", " "]
-#characterToCheck = ",;{()}\n\t= "
 
+# Defining the cleaning function:
 def clean(str, characterList):
+    """
+    This function trims trailing and leading whitespaces from a string and replace
+    any character of the string matching an element of a passed list with a "_"
+
+    Args:
+        str (str): a string to clean
+        characterList (list): a list of the character to replace
+
+    Returns:
+        x (str): a cleaned string
+    """
+    # Trimming the string
     x = str.strip()
+
+    # Looping over characterList and replace matching elements:
     for ch in characterList:
         x = x.replace(ch,"_")
     return x
 
-with open(filename, 'r') as f:
-    data = json.load(f)
+# Asking for JSON filename
+filename = input('JSON Filename?\n')
 
+# Opening JSON file and loading data:
+try:
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        f.close()
+# Exit from the script if file not found:
+except FileNotFoundError:
+    print('File not found!')
+    sys.exit()
+
+# Checking if 'source' field of any 'mappings' array element contains any undesired characters and 
+# calling the clean() function to clean it:
 for i in data['translator']['mappings']:
     if any(char in i['source'] for char in characterToCheck):
         i['sink'] = clean(i['sink'], characterToCheck)
-f.close()
 
+# Creating a new name for the modified file:
 filename_mod = str(filename) + '.New'
+
+# Loading the clean JSON code into the new file:
 with open(filename_mod, 'w') as f_mod:
     json.dump(data, f_mod, indent=4)
 f_mod.close()
